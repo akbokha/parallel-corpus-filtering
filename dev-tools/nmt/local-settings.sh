3904d469513cfc528ec036c6e42688234b32b2ee
akbokha <ak.bokharouss@gmail.com>
2019-06-10 15:50:38 +0100

    Update local settings with path-names of files/dirs on the valhalla cluster

--- dev-tools/nmt/local-settings.sh
+++ dev-tools/nmt/local-settings.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # set GPU as specified with 0 or dynamically
-export GPU=0
+export GPU=1
 
 # path to moses decoder: https://github.com/moses-smt/mosesdecoder
-export mosesdecoder=/home/pkoehn/moses
+export mosesdecoder=/fs/bil0/abdel/mosesdecoder
 
 # path to subword segmentation scripts: https://github.com/rsennrich/subword-nmt
-export subword_nmt=/home/pkoehn/statmt/project/subword-nmt
+export subword_nmt=/fs/bil0/abdel/subword-nmt
 
-# path to marian 
-export marian=/home/pkoehn/statmt/project/marian-v1.1
+# path to marian
+export marian=/fs/bil0/abdel/marian-dev
+
+# path to amun
+export amun=/fs/bil0/abdel/amun
 
 # path to clean eval dev sets
-export devset=/home/pkoehn/statmt/data/cleaneval/dev-tools/dev-sets
+export devset=/fs/bil0/abdel/data/dev
 
 # path to corpus to be tested
-export my_corpus_stem=/home/pkoehn/statmt/data/cleaneval/my-corpus
-
+export my_corpus_stem=/fs/bil0/abdel/data/train/parallel_corpus.10000000

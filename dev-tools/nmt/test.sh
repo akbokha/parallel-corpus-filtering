f12474553e641c506c67247960fc7a56178f430a
akbokha <ak.bokharouss@gmail.com>
2019-06-10 15:51:39 +0100

    Use marian-decoder instead of amun

--- dev-tools/nmt/test.sh
+++ dev-tools/nmt/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
 . ./local-settings.sh
 
 export BEST=`ls model/model.iter*.bleu | perl -ne 'chop; /iter(\d+).npz/; $iter = $1; qx/cat $_/ =~ /BLEU = ([\d\.]+), /; if ($1>$bleu) { $bleu=$1; $best = $iter; print $best."\n"; }' | tail -n 1`
 
-$marian/build/amun \
+$marian/build/marian-decoder \
     -d $GPU \
-    -n --wipo \
-    -s data/train.bpe.de.json \
-    -t data/train.bpe.en.json \
+    -n  \
+    -v data/train.bpe.de.json data/train.bpe.en.json \
     -m model/model.iter$BEST.npz \
     < data/test.bpe.de \
     > data/test.out

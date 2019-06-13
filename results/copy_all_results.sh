d9009d38efed5e19ebc8110ea62a683f4315b6c6
akbokha <ak.bokharouss@gmail.com>
2019-06-13 14:59:33 +0100

    fix issue in ls command and obsolete variable in the find command

--- results/copy_all_results.sh
+++ results/copy_all_results.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -v
 
-DIRS="$(ssh abdel@magni.inf.ed.ac.uk 'ls /fs/bil0/abdel/parallel-corpus-filtering/results')"
+DIRS="$(ssh abdel@magni.inf.ed.ac.uk 'ls -d /fs/bil0/abdel/parallel-corpus-filtering/results/*/ | grep -Eo '[^/]+/?$' | tail -1')"
 
 for dir in $DIRS ; do
     if ! test -f $dir/exp_files.tar.gz; then
         rsync -au --progress \
             abdel@magni.inf.ed.ac.uk:/fs/bil0/abdel/parallel-corpus-filtering/results/$dir .
-        find ./$EXP -name "*.bpe" \
+        find ./$dir -name "*.bpe" \
             -o -name "*.de" \
             -o -name "*.en" \
             -o -name "*.json" \

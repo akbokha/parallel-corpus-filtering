1c437c65f06c25a119eebd95a0a48c012dd3c960
akbokha <ak.bokharouss@gmail.com>
2019-06-14 16:51:24 +0100

    update test scripts with top-level dir var

--- dev-tools/test.sh
+++ dev-tools/test.sh
@@ -3,41 +3,40 @@ set -e
 
 . ./local-settings.sh
 
-export BEST=`ls model/model.iter*.bleu | perl -ne 'chop; /iter(\d+).npz/; $iter = $1; qx/cat $_/ =~ /BLEU = ([\d\.]+), /; if ($1>$bleu) { $bleu=$1; $best = $iter; print $best."\n"; }' | tail -n 1`
+export BEST=`ls $model_dir/model.iter*.bleu | perl -ne 'chop; /iter(\d+).npz/; $iter = $1; qx/cat $_/ =~ /BLEU = ([\d\.]+), /; if ($1>$bleu) { $bleu=$1; $best = $iter; print $best."\n"; }' | tail -n 1`
 
 $marian/build/marian-decoder \
     -d $GPU \
     -n  \
-    -v data/train.bpe.de.json data/train.bpe.en.json \
-    -m model/model.iter$BEST.npz \
-    < data/test.bpe.de \
-    > data/test.out
+    -v $data_dir/train.bpe.de.json $data_dir/train.bpe.en.json \
+    -m $model_dir/model.iter$BEST.npz \
+    < $data_dir/test.bpe.de \
+    > $data_dir/test.out
 
 sleep 5
 
-cat data/test.out \
+cat $data_dir/test.out \
     | sed -r 's/\@\@ //g' | sed -r 's/\@\@$//g' \
     | $mosesdecoder/scripts/tokenizer/detokenizer.perl -l en \
     | $mosesdecoder/scripts/recaser/detruecase.perl \
     | $mosesdecoder/scripts/ems/support/wrap-xml.perl en $devset/newstest2017-deen-src.de.sgm \
-    > data/test.out.sgm
+    > $data_dir/test.out.sgm
 
 $mosesdecoder/scripts/generic/mteval-v13a.pl \
     -c \
     -s $devset/newstest2017-deen-src.de.sgm \
     -r $devset/newstest2017-deen-ref.en.sgm \
-    -t data/test.out.sgm \
-    > data/test.out.bleu
+    -t $data_dir/test.out.sgm \
+    > $data_dir/test.out.bleu
 
-NOW=$(date +"%m_%d_%Y_%H%M%S")
-RESULTS_DIR=../results/$NOW
-mkdir -p $RESULTS_DIR/model
-mkdir -p $RESULTS_DIR/data
-
-cp model/model.iter$BEST.* $RESULTS_DIR/model
-cp model/model.npz*.yml $RESULTS_DIR/model
-cp model/*.log $RESULTS_DIR/model
-cp model/bleu_scores $RESULTS_DIR
-cp model/deen.bpe $RESULTS_DIR/model
-cp model/dev.out $RESULTS_DIR/model
-cp data/* $RESULTS_DIR/data
+# RESULTS_DIR=../results/$experiment
+# mkdir -p $RESULTS_DIR/model
+# mkdir -p $RESULTS_DIR/data
+#
+# cp $model_dir/model.iter$BEST.* $RESULTS_DIR/model
+# cp $model_dir/model.npz*.yml $RESULTS_DIR/model
+# cp $model_dir/*.log $RESULTS_DIR/model
+# cp $model_dir/bleu_scores $RESULTS_DIR
+# cp $model_dir/deen.bpe $RESULTS_DIR/model
+# cp $model_dir/dev.out $RESULTS_DIR/model
+# cp $model_dir/* $RESULTS_DIR/data

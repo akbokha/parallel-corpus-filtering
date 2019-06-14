68e04179bff46286628064a3361debe057c475cf
akbokha <ak.bokharouss@gmail.com>
2019-06-14 20:53:40 +0100

    revert and duplicate score dev changes

--- dev-tools/score-dev_exp.sh
+++ dev-tools/score-dev_exp.sh
@@ -2,15 +2,15 @@
 
 . ./local-settings.sh
 
-ref=data/dev.tok.en
+ref=$data_dir/dev.tok.en
 
-MODEL=`ls -t model/model.iter*npz | head -1`
+MODEL=`ls -t $model_dir/model.iter*npz | head -1`
 cp $1 $MODEL.bpe
 
 cat $1 \
     | sed 's/\@\@ //g' \
     | $mosesdecoder/scripts/generic/multi-bleu.perl $ref > $MODEL.bleu
 
-echo `date`" $MODEL: "`cat $MODEL.bleu` >> model/bleu_scores
+echo `date`" $MODEL: "`cat $MODEL.bleu` >> $model_dir/bleu_scores
 
 cat $MODEL.bleu | sed -r 's/BLEU = ([0-9.]+),.*/\1/'

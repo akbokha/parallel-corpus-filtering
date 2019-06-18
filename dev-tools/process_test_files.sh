05e9ed43de19d68d2793c667b064cf3b84c42ec3
akbokha <ak.bokharouss@gmail.com>
2019-06-18 13:28:25 +0100

    remove all the scripts that assumed the old directory structure

--- dev-tools/process_test_files.sh
+++ dev-tools/process_test_files.sh
@@ -9,25 +9,25 @@ TRG=en
 for test_set in $test_sets/*.$SRC; do
     test_file="$(basename "$test_set")"
     # wrap in xml
-    cat $test_set | $mosesdecoder/scripts/ems/support/create-xml.perl source > data/$test_file.sgm
+    cat $test_set | $mosesdecoder/scripts/ems/support/create-xml.perl source > $data_dir/$test_file.sgm
     # tokenize
     $mosesdecoder/scripts/tokenizer/normalize-punctuation.perl $SRC < $test_set \
-   	| $mosesdecoder/scripts/tokenizer/tokenizer.perl -a -l $SRC > data/"${test_file%.*}".tok.$SRC
+   	| $mosesdecoder/scripts/tokenizer/tokenizer.perl -a -l $SRC > $data_dir/"${test_file%.*}".tok.$SRC
     # truecase
-    $mosesdecoder/scripts/recaser/truecase.perl -model data/truecase-model.$SRC < data/"${test_file%.*}".tok.$SRC > data/"${test_file%.*}".tc.$SRC
+    $mosesdecoder/scripts/recaser/truecase.perl -model $data_dir/truecase-model.$SRC < $data_dir/"${test_file%.*}".tok.$SRC > $data_dir/"${test_file%.*}".tc.$SRC
     # apply BPE
-    $subword_nmt/apply_bpe.py -c model/$SRC$TRG.bpe < data/"${test_file%.*}".tc.$SRC > data/"${test_file%.*}".bpe.$SRC
+    $subword_nmt/apply_bpe.py -c $model_dir/$SRC$TRG.bpe < $data_dir/"${test_file%.*}".tc.$SRC > $data_dir/"${test_file%.*}".bpe.$SRC
 done
 
 for test_set in $test_sets/*.$TRG; do
     test_file="$(basename "$test_set")"
     # wrap in xml
-    cat $test_set | $mosesdecoder/scripts/ems/support/create-xml.perl ref > data/$test_file.sgm
+    cat $test_set | $mosesdecoder/scripts/ems/support/create-xml.perl ref > $data_dir/$test_file.sgm
     # tokenize
     $mosesdecoder/scripts/tokenizer/normalize-punctuation.perl $TRG < $test_set \
-   	| $mosesdecoder/scripts/tokenizer/tokenizer.perl -a -l $TRG > data/"${test_file%.*}".tok.$TRG
+   	| $mosesdecoder/scripts/tokenizer/tokenizer.perl -a -l $TRG > $data_dir/"${test_file%.*}".tok.$TRG
     # truecase
-    $mosesdecoder/scripts/recaser/truecase.perl -model data/truecase-model.$TRG < data/"${test_file%.*}".tok.$TRG > data/"${test_file%.*}".tc.$TRG
+    $mosesdecoder/scripts/recaser/truecase.perl -model $data_dir/truecase-model.$TRG < $data_dir/"${test_file%.*}".tok.$TRG > $data_dir/"${test_file%.*}".tc.$TRG
     # apply BPE
-    $subword_nmt/apply_bpe.py -c model/$SRC$TRG.bpe < data/"${test_file%.*}".tc.$TRG > data/"${test_file%.*}".bpe.$TRG
+    $subword_nmt/apply_bpe.py -c $model_dir/$SRC$TRG.bpe < $data_dir/"${test_file%.*}".tc.$TRG > $data_dir/"${test_file%.*}".bpe.$TRG
 done

3d61466131d1a946d27748ad98f624c532bcb006
akbokha <ak.bokharouss@gmail.com>
2019-07-07 15:24:53 +0100

    fix downloading of mono data

--- models/scripts/wmt2017-transformer/download-files-mono.sh
+++ models/scripts/wmt2017-transformer/download-files-mono.sh
@@ -2,8 +2,7 @@
 
 TRG=$1
 
-mkdir -p data
-cd data
+cd $data_dir
 
 # get En-De training data for WMT17
 wget -nc http://data.statmt.org/wmt17/translation-task/news.2016.$TRG.shuffled.gz
@@ -13,4 +12,4 @@ zcat news.2016.$TRG.shuffled.gz | shuf -n 11000000 | perl -ne 'print if(split(/\
 # clean
 rm -r news.2016.$TRG.shuffled.gz
 
-cd ..
+cd ../../../

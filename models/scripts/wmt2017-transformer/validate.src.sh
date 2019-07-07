6b83f54f1d28e74c5527bdbfe10aee82709d0b78
akbokha <ak.bokharouss@gmail.com>
2019-07-07 12:24:07 +0100

    Fix wrong moses_scripts paths in validation script

--- models/scripts/wmt2017-transformer/validate.src.sh
+++ models/scripts/wmt2017-transformer/validate.src.sh
@@ -7,7 +7,7 @@ data_dir=$2
 
 cat $1 \
     | sed 's/\@\@ //g' \
-    | $moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
-    | $moses-scripts/scripts/tokenizer/detokenizer.perl -l $SRC 2>/dev/null \
-    | $moses-scripts/scripts/generic/multi-bleu-detok.perl $data_dir/valid.$SRC \
+    | $moses_scripts/scripts/recaser/detruecase.perl 2>/dev/null \
+    | $moses_scripts/scripts/tokenizer/detokenizer.perl -l $SRC 2>/dev/null \
+    | $moses_scripts/scripts/generic/multi-bleu-detok.perl $data_dir/valid.$SRC \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'

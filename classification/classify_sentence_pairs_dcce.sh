55f5be06043a82235b6b72c4815d8276d69a0a0b
akbokha <ak.bokharouss@gmail.com>
2019-07-24 00:30:05 +0100

    add scripts for bicleaner_dcce classification

---
+++ classification/classify_sentence_pairs_dcce.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+EXPERIMENT=${1:-"bicleaner_v1.1"}
+TRAINING_METADATA_DIR=${2:-"../data/language_packs/en-de"}
+
+OUTPUT_DIR="./output/$EXPERIMENT"
+
+TO_BE_CLASSIFIED_DATA=${3:-"../hardrules/output/$EXPERIMENT/data_hardrules_filtered"}
+
+GPUS=${4:-"0"}
+
+model_dir_src_trg="../models/translation/models/transformer-model-$SRC-$TRG"/model
+model_dir_trg_src="../models/translation/models/transformer-model-$TRG-$SRC"/model
+
+BEST_SRC_TRG=`cat $model_dir_src_trg/valid.log | grep bleu | sort -rg -k12,12 -t' ' | cut -f8 -d' ' | head -n1`
+BEST_TRG_SRC=`cat $model_dir_trg_src/valid.log | grep bleu | sort -rg -k12,12 -t' ' | cut -f8 -d' ' | head -n1`
+
+mkdir -p $OUTPUT_DIR
+
+bicleaner-classify  \
+        $TO_BE_CLASSIFIED_DATA  \
+        $OUTPUT_DIR/data_classified  \
+        $TRAINING_METADATA_DIR/en-de.yaml \
+        --threshold 0.5 \
+        --gpu 0 \
+        --dcce_model_src_trg $model_dir_src_trg/model.iter$BEST_SRC_TRG.npz \
+        --dcce_model_trg_src $model_dir_trg_src/model.iter$BEST_TRG_SRC.npz \
+        --dcce_src_vocab_src_trg $model_dir_src_trg/vocab.ende.yml \
+        --dcce_trg_vocab_src_trg $model_dir_src_trg/vocab.ende.yml \
+        --dcce_src_vocab_trg_src $model_dir_trg_src/vocab.ende.yml \
+        --dcce_trg_vocab_trg_src $model_dir_trg_src/vocab.ende.yml
+
+python extract_corpora_and_scores.py $OUTPUT_DIR
+
+rm $OUTPUT_DIR/data_classified
---
+++ classification/scripts/dcce_scoring.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+marian=/fs/bil0/abdel/marian
+marian_scorer=$marian/build/marian-scorer
+
+model=$1
+
+src_sentences=$2
+trg_sentences=$3
+
+src_vocab=$4
+trg_vocab=$5
+
+gpus=$6
+
+$marian_scorer \
+    -m $model \
+    -v $src_vocab $trg_vocab \
+    -t $src_sentences $trg_sentences \
+    --devices "$gpus" \
+    --workspace 4096 \
+    --quiet-translation \
+    -n

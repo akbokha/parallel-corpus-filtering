f2cdc4201b44a6f711b8a981e4e189c2795238d4
akbokha <ak.bokharouss@gmail.com>
2019-08-02 19:09:07 +0100

    scores and log files of experimental results

---
+++ experiments/100M_fasttext_prob_x_len_+_dcce_x_ced_0.0/score-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. ./local-settings.sh
+data_dir=../experiments/100M_fasttext_prob_x_len_+_dcce_x_ced_0.0/data
+model_dir=../experiments/100M_fasttext_prob_x_len_+_dcce_x_ced_0.0/model
+
+ref=$data_dir/dev.tok.en
+
+MODEL=`ls -t $model_dir/model.iter*npz | head -1`
+cp $1 $MODEL.bpe
+
+cat $1 \
+    | sed 's/\@\@ //g' \
+    | $mosesdecoder/scripts/generic/multi-bleu.perl $ref > $MODEL.bleu
+
+echo `date`" $MODEL: "`cat $MODEL.bleu` >> $model_dir/bleu_scores
+
+cat $MODEL.bleu | sed -r 's/BLEU = ([0-9.]+),.*/\1/'

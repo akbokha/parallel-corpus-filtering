6c976bc8dda2493c326b793d3363d51dbc6ce0ac
akbokha <ak.bokharouss@gmail.com>
2019-08-07 16:00:42 +0000

    update experiment logs after downtime

---
+++ experiments/10M_fasttext_x_bic1.1_+_bic1.1_no_lm_x_biced_0.25/score-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. ./local-settings.sh
+data_dir=../experiments/10M_fasttext_x_bic1.1_+_bic1.1_no_lm_x_biced_0.25/data
+model_dir=../experiments/10M_fasttext_x_bic1.1_+_bic1.1_no_lm_x_biced_0.25/model
+
+ref=$data_dir/dev.tok.en
+
+MODEL=`ls -t $model_dir/model.iter*npz | head -1`
+cp $1 $MODEL.bpe
+
+cat $1 \
+    | sed 's/\@\@ //g' \
+    | $mosesdecoder/scripts/generic/multi-bleu.perl $ref > $MODEL.bleu
+
+echo `date`" $MODEL: "`cat $MODEL.bleu` >> $model_dir/bleu_scores
+
+cat $MODEL.bleu | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
